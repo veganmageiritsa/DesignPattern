8e1f29e5ba6fd78c5a7ed98b46328c9a81eb93f0
nikolas <nikolas.leontiou@vodafoneinnovus.com>
2019-03-29 14:00:23 +0200

    Change name of project to DesignPatterns

--- src/com/behavioral/chain_responsibility/Lead.java
+++ src/com/behavioral/chain_responsibility/Lead.java
@@ -2,6 +2,9 @@ package com.behavioral.chain_responsibility;
 
 public class Lead extends Employee {
 
+    public Lead(String name, String role, LeaveApprover successor) {
+        super(name, role, successor);
+    }
 
     @Override
     protected boolean processRequest(LeaveApplication application) {
